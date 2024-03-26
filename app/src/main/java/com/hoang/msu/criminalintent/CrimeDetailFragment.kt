687e808f96bbc6fcd6a629edf99c8be0fcf68220
Caylipp <philipphoang3005@gmail.com>
2024-03-26 11:21:02 -0500

    add func

---
+++ app/src/main/java/com/hoang/msu/criminalintent/CrimeDetailFragment.kt
@@ -0,0 +1,62 @@
+package com.hoang.msu.criminalintent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import com.hoang.msu.criminalintent.databinding.FragmentCrimeDetailBinding
+import java.util.Date
+import java.util.UUID
+
+class CrimeDetailFragment: Fragment() {
+
+    private lateinit var crime: Crime
+    private lateinit var binding: FragmentCrimeDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        crime = Crime(
+            UUID.randomUUID(),
+            title = "",
+            date = Date(),
+            isSolved = false
+        )
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            //listener edit text
+            crimeTitle.doOnTextChanged{text,_,_,_ ->
+                crime = crime.copy(title = text.toString())
+            }
+            //listener edit text
+            crimeDate.apply {
+                text = crime.date.toString()
+                isEnabled = false
+
+            }
+            crimeSolved.setOnCheckedChangeListener { _, isChecked ->
+                crime = crime.copy(isSolved = isChecked)
+            }
+        }
+
+    }
+
+
+}
