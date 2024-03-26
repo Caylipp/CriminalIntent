ca5f6fb6c8a502be5bb3565666aea75317fae6fc
Caylipp <philipphoang3005@gmail.com>
2024-03-26 10:46:31 -0500

    Init Commit

---
+++ app/src/main/java/com/hoang/msu/criminalintent/Crime.kt
@@ -0,0 +1,12 @@
+package com.hoang.msu.criminalintent
+
+import java.util.Date
+import java.util.UUID
+
+data class Crime (
+
+    val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean
+)
