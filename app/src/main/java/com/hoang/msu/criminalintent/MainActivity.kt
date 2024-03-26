ed44831c1161f11dcacf2f3cff8dcd1df6c0663c
Caylipp <philipphoang3005@gmail.com>
2024-03-26 16:03:05 -0500

    Ex 2a init

--- app/src/main/java/com/hoang/msu/criminalintent/MainActivity.kt
+++ app/src/main/java/com/hoang/msu/criminalintent/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hoang.msu.criminalintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
