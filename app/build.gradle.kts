703df1755a4ea2bb5432d1ef0d50e7c48eea55e1
Caylipp <philipphoang3005@gmail.com>
2024-03-26 16:35:43 -0500

    Ex 2a implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,4 +49,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("androidx.fragment:fragment-ktx:1.4.1")
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
+
 }
