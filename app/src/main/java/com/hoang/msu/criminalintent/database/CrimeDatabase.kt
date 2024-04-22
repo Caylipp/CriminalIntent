0806072d594a8d7e934c6fd9c5c623c38b850f31
Caylipp <philipphoang3005@gmail.com>
2024-04-21 20:55:28 -0500

    Ch12-1a
    -add

--- app/src/main/java/com/hoang/msu/criminalintent/database/CrimeDatabase.kt
+++ app/src/main/java/com/hoang/msu/criminalintent/database/CrimeDatabase.kt
@@ -1,4 +1,14 @@
 package com.hoang.msu.criminalintent.database
 
-class CrimeDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.hoang.msu.criminalintent.Crime
+
+
+@Database(entities = [ Crime::class ], version=1)
+@TypeConverters(CrimeTypeConverters::class)
+abstract class CrimeDatabase : RoomDatabase() {
+    abstract fun crimeDao(): CrimeDao
 }
+
